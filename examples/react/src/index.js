fcd874427c922241befd2281c75d8d7b6d44179a
Jan Nicklas <j.nicklas@me.com>
2018-08-15 13:14:34 +0200

    docs: Add readme for the react example (#12)

--- examples/react/src/index.js
+++ examples/react/src/index.js
@@ -1,13 +1,11 @@
-import * as ReactDOM from 'react-dom';
+import { startComponents } from '@gondel/core';
+import { hot } from '@gondel/plugin-hot';
 import * as React from 'react';
-import {startComponents} from '@gondel/core';
-import {hot} from '@gondel/plugin-hot';
-import {initEventPlugin} from '@gondel/plugin-events';
+import * as ReactDOM from 'react-dom';
 import App from './app.jsx';
 
 hot(module);
-initEventPlugin();
-
 startComponents();
+
 ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
 
