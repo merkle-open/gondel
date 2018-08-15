3140f7d92f51ce038b0bccc4d64e4e0febbeffc5
Jan Nicklas <j.nicklas@me.com>
2018-08-15 15:53:47 +0200

    docs: Add typescript sandbox link

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,12 +1,11 @@
 import { startComponents } from "@gondel/core";
-import { initEventPlugin } from "@gondel/plugin-events";
 import { hot } from "@gondel/plugin-hot";
 declare const module: any;
 hot(module);
-initEventPlugin();
 
-// Load all components
-const componentsContext = (require as any).context("./components", true, /\.ts$/);
-componentsContext.keys().forEach(componentsContext);
+// Load components
+import("./components/button");
+import("./components/form");
+import("./components/input");
 
 startComponents();
