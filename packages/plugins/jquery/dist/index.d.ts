504bdf49514a52151970b15a7960febe37cb3607
Jan Nicklas <j.nicklas@me.com>
2018-09-06 14:04:34 +0200

    feat(plugin-jquery): Provide a mixin and a baseClass

--- packages/plugins/jquery/dist/index.d.ts
+++ packages/plugins/jquery/dist/index.d.ts
@@ -1,1 +1,33 @@
-export declare function init(): void;
+/// <reference types="jquery" />
+/**
+ * This plugin provides utils for Gondel jQuery integrations
+ */
+import { GondelBaseComponent } from "@gondel/core";
+/**
+ * Class mixin https://basarat.gitbooks.io/typescript/docs/types/mixins.html
+ * Usage:
+   ```
+class MyComponent extends gondelJQueryMixin(GondelBaseComponent) {
+  start() {
+    console.log(this.$ctx);
+  }
+}
+   ```
+ */
+export declare function gondelJQueryMixin<T extends GondelBaseComponent, U extends new (context: HTMLElement, componentName: string) => T>(BaseClass: U): new (context: HTMLElement, componentName: string) => T & {
+    $ctx: JQuery<HTMLElement>;
+};
+/**
+ * BaseComponent with JqueryMixin
+  * Usage:
+   ```
+class MyComponent extends GondelJqueyComponent {
+  start() {
+    console.log(this.$ctx);
+  }
+}
+   ```
+ */
+export declare const GondelJqueyComponent: new (context: HTMLElement, componentName: string) => GondelBaseComponent & {
+    $ctx: JQuery<HTMLElement>;
+};
