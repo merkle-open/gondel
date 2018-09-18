dce8fd665dfca93ebf78076dbef22b9e145c4c55
Jan Nicklas <j.nicklas@me.com>
2018-09-18 09:13:52 +0200

    docs: Add five-star example

---
+++ examples/five-star/readme.md
@@ -0,0 +1,7 @@
+# 🚡 Gondel Five Star Example
+
+In this example we use an ordinary input range slider as a five star rating widget.
+
+Try it your self:
+
+https://codesandbox.io/s/github/namics/gondel/tree/master/examples/five-star
---
+++ examples/five-star/webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CommonConfigWebpackPlugin = require('common-config-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  context: __dirname,
+  entry: './src/index.js',
+  plugins: [
+    new CommonConfigWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+};
