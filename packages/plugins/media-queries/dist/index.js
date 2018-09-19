3737d56cd15517fb44e00f5cab96b2aa21900a31
Jan Nicklas <j.nicklas@me.com>
2018-09-19 11:09:35 +0200

    chore: Rebuild components

--- packages/plugins/media-queries/dist/index.js
+++ packages/plugins/media-queries/dist/index.js
@@ -69,7 +69,7 @@ function convertBreakpointsToEm(breakpointsInPx) {
  * Convert pixel to em
  */
 function px2em(pixelValue) {
-    return Math.round(pixelValue / 16 * 100) / 1000;
+    return Math.round((pixelValue / 16) * 100) / 1000;
 }
 /**
  * This function generate mediaQueries from breakPoints
