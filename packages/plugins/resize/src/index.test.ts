e19da20a7b163d7a0850c9609d4f07f684cbe873
Jan Widmer <jan.widmer@namics.com>
2019-03-01 08:07:28 +0100

    feat(resize plugin): add jest test config and first test

---
+++ packages/plugins/resize/src/index.test.ts
@@ -0,0 +1,88 @@
+import { GondelBaseComponent, IGondelComponent } from "../../../core/src/GondelComponent";
+import {
+  startComponents,
+  getComponentByDomNode,
+  Component,
+  EventListener
+} from "../../../core/src/index";
+import { disableAutoStart } from "../../../core/src/GondelAutoStart";
+import { initResizePlugin, WINDOW_RESIZED_EVENT, COMPONENT_RESIZED_EVENT } from "./index";
+
+function resize(width: number, height: number) {
+  // Simulate window resize event
+  const resizeEvent = document.createEvent("Event");
+  resizeEvent.initEvent("resize", true, true);
+  (window as any).innerWidth = width || window.innerWidth;
+  (window as any).innerHeight = height || window.innerHeight;
+
+  window.dispatchEvent(resizeEvent);
+}
+
+function createMockElement(namespace: string) {
+  const divElement = document.createElement("div");
+  divElement.innerHTML = `<p>This is a container being resized</p>`;
+  divElement.setAttribute("data-" + namespace + "-name", "ResizeComponent");
+
+  document.documentElement.appendChild(divElement);
+
+  startComponents(divElement, namespace);
+
+  return getComponentByDomNode(divElement, namespace)!;
+}
+
+disableAutoStart();
+initResizePlugin();
+
+@Component("ResizeComponent")
+class ResizeComponent extends GondelBaseComponent {
+  _componentWidth = 0;
+  _componentHeight = 0;
+  _windowResizedEventReceived = false;
+  _componentResizedEventReceived = false;
+  public start() {}
+  public getWindowResizeEventReceived(): boolean {
+    return this._windowResizedEventReceived;
+  }
+
+  public getComponentResizeEventReceived(): boolean {
+    return this._componentResizedEventReceived;
+  }
+  public getWidth(): number {
+    return this._componentWidth;
+  }
+  public getHeight(): number {
+    return this._componentHeight;
+  }
+  @EventListener(WINDOW_RESIZED_EVENT)
+  public _handleWindowResizeEvent() {
+    console.log("window resize happened");
+    this._windowResizedEventReceived = true;
+  }
+  @EventListener(COMPONENT_RESIZED_EVENT)
+  public _handleComponentResizeEvent() {
+    console.log("component resize happened");
+    this._componentResizedEventReceived = true;
+  }
+  private setDimensions() {
+    this._componentWidth = this._ctx.offsetWidth;
+    this._componentHeight = this._ctx.offsetHeight;
+  }
+}
+
+describe("GondelResizePlugin", () => {
+  it("should receive an window resized event upon resize", () => {
+    resize(1400, 800);
+    const component = createMockElement("g") as ResizeComponent;
+    resize(1200, 600);
+
+    expect(component.getWindowResizeEventReceived()).toBe(true);
+  });
+
+  /*it("should receive an component resized event upon resize", () => {
+    resize(1400, 800);
+    const component = createMockElement('g') as ResizeComponent;
+    resize(1200, 600);
+
+    expect(component.getComponentResizeEventReceived()).toBe(true);
+  });*/
+});
