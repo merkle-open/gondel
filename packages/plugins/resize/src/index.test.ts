c425357c5e203426e31586c4d31a22192ba8b4b5
Jan Widmer <jan.widmer@namics.com>
2019-03-01 09:45:39 +0100

    feat(resize plugin): add logic to override clientWidth and clientHeight

--- packages/plugins/resize/src/index.test.ts
+++ packages/plugins/resize/src/index.test.ts
@@ -8,13 +8,26 @@ import {
 import { disableAutoStart } from "../../../core/src/GondelAutoStart";
 import { initResizePlugin, WINDOW_RESIZED_EVENT, COMPONENT_RESIZED_EVENT } from "./index";
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const { JSDOM } = require("jsdom");
 
 const dom = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>");
 
+// assigment needed to make gondel init and event logic work
 window = dom.window;
 document = dom.window.document;
+
+// mock clientWidth and clientHeight, see https://github.com/jsdom/jsdom/issues/2342
+Object.defineProperty((window as any).HTMLElement.prototype, "clientWidth", {
+  get: function() {
+    return this._jsdomMockClientWidth || 0;
+  }
+});
+
+Object.defineProperty((window as any).HTMLElement.prototype, "clientHeight", {
+  get: function() {
+    return this._jsdomMockClientHeight || 0;
+  }
+});
 
 function resize(width: number, height: number) {
   // Simulate window resize event
@@ -27,13 +40,8 @@ function resize(width: number, height: number) {
 }
 
 function createMockElement(namespace: string) {
-  const divElement = document.createElement("div");
-  divElement.innerHTML = `<p>This is a container being resized</p>`;
-  divElement.setAttribute("data-" + namespace + "-name", "ResizeComponent");
-
-  window.document.body.appendChild(divElement);
-
-  return divElement;
+  window.document.body.innerHTML = `<div id="resize-component" data-${namespace}-name="ResizeComponent"><p>This is a container being resized</p></div>`;
+  return window.document.getElementById("resize-component");
 }
 
 function getGondelComponent(namespace: string, divElement: HTMLElement): ResizeComponent {
@@ -83,18 +91,30 @@ class ResizeComponent extends GondelBaseComponent {
 describe("GondelResizePlugin", () => {
   it("should receive an window resized event upon resize", () => {
     const divElement = createMockElement("g");
-    const component = getGondelComponent("g", divElement);
+    const component = getGondelComponent("g", divElement!);
 
     resize(1200, 600);
 
     expect(component.getWindowResizeEventReceived()).toBe(true);
   });
 
-  /*it("should receive an component resized event upon resize", () => {
-    resize(1400, 800);
-    const component = createMockElement('g') as ResizeComponent;
+  /* it("should receive an component resized event upon resize", () => {
+    const divElement = createMockElement("g");
+
+    debugger;
+
+    const test = window.document.querySelector("div");
+
+    (test as any)._jsdomMockClientWidth = 1400;
+    (test as any)._jsdomMockClientHeight = 600;
+
+    console.log(`Client Width: ${test!.clientWidth}`);
+    console.log(`Client Height: ${test!.clientHeight}`);
+
+    const component = getGondelComponent("g", divElement!);
+
     resize(1200, 600);
 
     expect(component.getComponentResizeEventReceived()).toBe(true);
-  });*/
+  }); */
 });
