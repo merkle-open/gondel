147bbb7edd84acd0a1dd8278a43615724f888774
Jan Widmer <jan.widmer@namics.com>
2019-03-04 15:15:36 +0100

    feat(resize plugin): run tests synchronous and add more tests

--- packages/plugins/resize/src/index.test.ts
+++ packages/plugins/resize/src/index.test.ts
@@ -1,37 +1,31 @@
-import { GondelBaseComponent, IGondelComponent } from "../../../core/src/GondelComponent";
 import {
+  GondelBaseComponent,
   startComponents,
   stopComponents,
   getComponentByDomNode,
   Component,
-  EventListener
-} from "../../../core/src/index";
-import { disableAutoStart } from "../../../core/src/GondelAutoStart";
+  EventListener,
+  disableAutoStart
+} from "@gondel/core";
+
 import { initResizePlugin, WINDOW_RESIZED_EVENT, COMPONENT_RESIZED_EVENT } from "./index";
 
-const { JSDOM } = require("jsdom");
+// mock clientWidth and clientHeight, see https://github.com/jsdom/jsdom/issues/2342
+Object.defineProperty((window as any).HTMLElement.prototype, "clientWidth", {
+  get: function() {
+    return this._jsdomMockClientWidth || 0;
+  }
+});
 
-function createJsDom() {
-  const dom = new JSDOM("<!DOCTYPE html><html><head></head><body></body></html>");
+Object.defineProperty((window as any).HTMLElement.prototype, "clientHeight", {
+  get: function() {
+    return this._jsdomMockClientHeight || 0;
+  }
+});
 
-  // assigment needed to make gondel init and event logic work
-  window = dom.window;
-  document = dom.window.document;
-}
-
-function overrideClientWidthAndHeight() {
-  // mock clientWidth and clientHeight, see https://github.com/jsdom/jsdom/issues/2342
-  Object.defineProperty((window as any).HTMLElement.prototype, "clientWidth", {
-    get: function() {
-      return this._jsdomMockClientWidth || 0;
-    }
-  });
-
-  Object.defineProperty((window as any).HTMLElement.prototype, "clientHeight", {
-    get: function() {
-      return this._jsdomMockClientHeight || 0;
-    }
-  });
+function setMockClientWidthAndHeight(component: HTMLElement, width: number, height: number) {
+  (component as any)._jsdomMockClientWidth = width;
+  (component as any)._jsdomMockClientHeight = height;
 }
 
 function resize(width: number, height: number) {
@@ -44,45 +38,21 @@ function resize(width: number, height: number) {
   window.dispatchEvent(resizeEvent);
 }
 
-function createMockElement(namespace: string) {
-  window.document.body.innerHTML = `<div id="resize-component" data-${namespace}-name="ResizeComponent"><p>This is a container being resized</p></div>`;
-  return window.document.getElementById("resize-component");
-}
-
-function getGondelComponent(namespace: string, divElement: HTMLElement): ResizeComponent {
-  startComponents(divElement, namespace);
-  return getComponentByDomNode(divElement, namespace);
-}
-
 @Component("ResizeComponent")
 class ResizeComponent extends GondelBaseComponent {
-  _windowResizedEventReceived = false;
-  _componentResizedEventReceived = false;
-  _numberOfComponentResizeEventsHappened = 0;
+  _windowResizedEventReceived = 0;
+  _componentResizedEventReceived = 0;
 
   public start() {}
 
-  public sync() {
-    console.log("Component sync");
-  }
+  public sync() {}
 
-  public getWindowResizeEventReceived(): boolean {
+  public getWindowResizeEventReceived(): number {
     return this._windowResizedEventReceived;
   }
 
-  public getComponentResizeEventReceived(): boolean {
+  public getComponentResizeEventReceived(): number {
     return this._componentResizedEventReceived;
-  }
-
-  public getNumberOfComponentResizeEventsHappened(): number {
-    return this._numberOfComponentResizeEventsHappened;
-  }
-
-  public getWidth(): number {
-    return this._ctx.clientWidth;
-  }
-  public getHeight(): number {
-    return this._ctx.clientHeight;
   }
 
   public setDimensions(width: number, height: number) {
@@ -92,76 +62,157 @@ class ResizeComponent extends GondelBaseComponent {
 
   @EventListener(WINDOW_RESIZED_EVENT)
   public _handleWindowResizeEvent() {
-    console.log("window resize happened");
-    this._windowResizedEventReceived = true;
+    this._windowResizedEventReceived++;
   }
   @EventListener(COMPONENT_RESIZED_EVENT)
   public _handleComponentResizeEvent() {
-    console.log("component resize happened");
-    this._componentResizedEventReceived = true;
-    this._numberOfComponentResizeEventsHappened++;
+    this._componentResizedEventReceived++;
   }
 }
+
+let gondelDivElement: HTMLDivElement;
+let gondelInstance: ResizeComponent;
 
 describe("GondelResizePlugin", () => {
   beforeEach(() => {
     disableAutoStart();
-    createJsDom();
-    overrideClientWidthAndHeight();
+
+    const mockElementWrapper = document.createElement("div");
+    mockElementWrapper.innerHTML = `<div data-g-name="ResizeComponent"><p>This is a container being resized</p></div>`;
+    gondelDivElement = mockElementWrapper.firstElementChild as HTMLDivElement;
+    document.body.appendChild(gondelDivElement);
+
     initResizePlugin();
+    startComponents(gondelDivElement);
+
+    gondelInstance = getComponentByDomNode(gondelDivElement);
   });
 
   afterEach(() => {
-    stopComponents(window.document.getElementById("resize-component")!);
-    window.document.body.innerHTML = "";
-
-    (document as any) = undefined;
-    (window as any) = undefined;
+    stopComponents(document.body);
+    document.body.innerHTML = "";
   });
 
-  /*it("should receive an window resized event upon resize", () => {
-    const divElement = createMockElement("g");
-    const component = getGondelComponent("g", divElement!);
+  it("should receive no window resized event without resize", () => {
+    expect(gondelInstance.getWindowResizeEventReceived()).toBe(0);
+  });
 
+  it("should receive no component resized event without resize", () => {
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(0);
+  });
+
+  it("should receive an window resized event upon resize", async () => {
     resize(1200, 600);
+    await new Promise(resolve => setTimeout(resolve, 300));
+    expect(gondelInstance.getWindowResizeEventReceived()).toBe(1);
+  });
 
-    expect(component.getWindowResizeEventReceived()).toBe(true);
-  });*/
-
-  /*it("should not receive an component resized event if the component's dimension did not change", () => {
-    const divElement = createMockElement("g");
-    const component = getGondelComponent("g", divElement!);
-
-    expect(component.getComponentResizeEventReceived()).toBe(false);
-  });*/
-
-  /*it("should receive an component resized event if the component's dimension changed", () => {
-    const divElement = createMockElement("g");
-
-    const component = getGondelComponent("g", divElement!);
-    component.setDimensions(1400, 600);
-
-    console.log(`Client Width: ${component.getWidth()}`);
-    console.log(`Client Height: ${component.getHeight()}`);
-
-    resize(1200, 400);
-
-    // TODO should fail, but works because initial width and height are 0.. (because sync event seems not working)
-    expect(component.getComponentResizeEventReceived()).toBe(true);
-  });*/
-
-  it("should trigger 2 component resize events with a timeout of > 250ms", async () => {
-    const divElement = createMockElement("g");
-
-    const component = getGondelComponent("g", divElement!);
-    component.setDimensions(1400, 600);
-
+  it("should receive no component resized event when component dimensions did not change", async () => {
     resize(1200, 400);
     await new Promise(resolve => setTimeout(resolve, 300));
-    // TODO why does the test fail if dimensions are not set again (according to code, dimensions should be reset after 250ms)
-    // component.setDimensions(1400, 600);
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(0);
+  });
+
+  it("should receive a component resized event when component dimensions did change", async () => {
+    gondelInstance.setDimensions(1400, 600);
+    resize(1400, 600);
+    await new Promise(resolve => setTimeout(resolve, 300));
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(1);
+  });
+
+  it("should receive two component resize events when firing resize two times", async () => {
+    // we need to set the initial dimensions as they are 0 when the resize plugin get's initialized
+    gondelInstance.setDimensions(1400, 600);
+
+    // first resize event get's executed right away
+    resize(1400, 600);
+
+    gondelInstance.setDimensions(1300, 500);
+
+    resize(1300, 500);
+
+    await new Promise(resolve => setTimeout(resolve, 300));
+
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(2);
+  });
+
+  it("should should throttle more than 2 resize events being fired", async () => {
+    // we need to set the initial dimensions as they are 0 when the resize plugin get's initialized
+    gondelInstance.setDimensions(1400, 600);
+
+    // first resize event get's executed right away
+    resize(1400, 600);
+
+    gondelInstance.setDimensions(1300, 500);
+    // second resize get's stored for requestAnimationFrame
+    resize(1300, 500);
+
+    // further resize events get neglected
+    gondelInstance.setDimensions(1200, 400);
+    resize(1200, 400);
+
+    gondelInstance.setDimensions(1100, 300);
     resize(1100, 300);
 
-    expect(component.getNumberOfComponentResizeEventsHappened()).toBe(2);
+    gondelInstance.setDimensions(1000, 200);
+    resize(1000, 200);
+
+    await new Promise(resolve => setTimeout(resolve, 300));
+
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(2);
+  });
+
+  it("should should reset throttling after requestAnimationFrame time duration", async () => {
+    // we need to set the initial dimensions as they are 0 when the resize plugin get's initialized
+    gondelInstance.setDimensions(1400, 600);
+
+    // first resize event get's executed right away
+    resize(1400, 600);
+
+    gondelInstance.setDimensions(1300, 500);
+    resize(1300, 500);
+
+    await new Promise(resolve => setTimeout(resolve, 20));
+
+    gondelInstance.setDimensions(1200, 400);
+    resize(1200, 400);
+
+    await new Promise(resolve => setTimeout(resolve, 20));
+
+    gondelInstance.setDimensions(1100, 300);
+    resize(1100, 300);
+
+    await new Promise(resolve => setTimeout(resolve, 20));
+
+    gondelInstance.setDimensions(1000, 200);
+    resize(1000, 200);
+
+    await new Promise(resolve => setTimeout(resolve, 300));
+
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(5);
+  });
+
+  it("should should reset component information after 250ms time duration", async () => {
+    // we need to set the initial dimensions as they are 0 when the resize plugin get's initialized
+    gondelInstance.setDimensions(1400, 600);
+
+    // first resize event get's executed right away
+    resize(1400, 600);
+
+    gondelInstance.setDimensions(1300, 500);
+    resize(1300, 500);
+
+    // after 250ms, the resize event is being considered completed and component information get reset
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    // resetting __resizeSize forces to set width / height to 0 and therefore being different than previous dimensions
+    (gondelInstance as any).__resizeSize = undefined;
+
+    // next resize event get's treated as fresh resize and therefor also triggers component resize event
+    resize(1200, 400);
+
+    await new Promise(resolve => setTimeout(resolve, 300));
+
+    expect(gondelInstance.getComponentResizeEventReceived()).toBe(3);
   });
 });
