6f0ca1515ae987dceec3ec7aea85399b2514be4a
ernscht <ernscht@gmail.com>
2019-12-23 13:15:24 +0100

    chore: update dist files

--- packages/core/dist/GondelDecorators.d.ts
+++ packages/core/dist/GondelDecorators.d.ts
@@ -1,8 +1,10 @@
 import { GondelComponent, IGondelComponent } from "./GondelComponent";
 export declare function Component(componentName: string, namespace?: string): (constructor: IGondelComponent) => void;
+declare type EventOption = [string, string, string | object | undefined];
 /**
  * The @EventListener decorator will add all event names to a static variable
  */
 export declare function EventListener(eventName: string, selector?: string | object): <T extends {
-    __events?: [string, string, string | object | undefined][] | undefined;
+    __events?: EventOption[] | undefined;
 } & GondelComponent<HTMLElement>>(target: T, handler: string) => void;
+export {};
