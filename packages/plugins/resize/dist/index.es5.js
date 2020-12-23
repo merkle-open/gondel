59a9482886795403dc3102ec11398143202b1b4d
ernscht <ernscht@gmail.com>
2020-12-23 14:49:20 +0100

    chore: update dev deps

--- packages/plugins/resize/dist/index.es5.js
+++ packages/plugins/resize/dist/index.es5.js
@@ -2,7 +2,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = global || self, factory(global.gondelPluginResize = {}));
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.gondelPluginResize = {}));
 }(this, (function (exports) { 'use strict';
 
     var basePluginListener = function (result, data, next) { return next(result); };
