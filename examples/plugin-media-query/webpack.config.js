b358eaecabc8123f1bf07840f928ecd31ff405cd
ernscht <ernscht@gmail.com>
2023-01-16 10:06:13 +0100

    chore: avoid md4 hash for OpenSSL 3 compatibility

--- examples/plugin-media-query/webpack.config.js
+++ examples/plugin-media-query/webpack.config.js
@@ -1,6 +1,11 @@
 const JsConfigWebpackPlugin = require('js-config-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+// hack: OpenSSL 3 does not support md4 anymore, but legacy webpack 4 hardcoded it: https://github.com/webpack/webpack/issues/13572
+const crypto = require('crypto');
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
